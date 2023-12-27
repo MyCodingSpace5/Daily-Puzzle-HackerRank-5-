e3ff2c4fa68b591518ea766afd30a7287b022e7f
Ahmet <121121504+MyCodingSpace5@users.noreply.github.com>
2023-12-26 21:16:40 -0600

    Update king.py

--- king.py
+++ king.py
@@ -1,12 +1,30 @@
-def element(array, x, y, string)
-    conversionrate = 0
+def conversionelement(array, conversion1, conversion2, conversionrate, array, functionlayout)
+    arrayconversionrate = []
+    symbolconversionrate = []
     newArray = []
+    sum = 0
     for i in string:
-        if(int(i) * 2 == i * 2):
-            conversionrate = i
-        else:
-            continue
-    for i in array:
-        array.remove(i)
-        newArray.append(i * conversionrate)
-    return newArray
+        try:
+            i = int(i)
+        except ValueError:
+            symbolconversionrate.append(i)
+        
+        for item in newArray:
+            for i in range(len(arrayconversionrate)):
+                if(symbolconversionrate[i] == "+"):
+                    newArray.remove(item)
+                    item = item + arrayconversionrate[i]
+                    newArray.append(item)
+                 if(symbolconversionrate[i] == "-"):
+                     newArray.remove(item)
+                     item = item - arrayconversionrate[i]
+                     newArray.append(item)
+                 if(symbolconversionrate[i] == conversion1):
+                     newArray.remove(item)
+                     item = item * arrayconversionrate[i]
+                     newArray.append(item) 
+
+        for item in newArray:
+            sum += item
+    return str(newArray) + "Finished Calculation" + "Total amount of" + conversion2 + "  is" + sum
+
